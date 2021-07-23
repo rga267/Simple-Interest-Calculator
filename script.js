8be67978fb07f29e1a701dada5c0cefccd7e822b
Ricardo Gutierrez <rga267@nyu.edu>
2021-07-23 16:31:03 -0400

    handling error message for negative input

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
+
 function compute()
-{
-    var principal = document.getElementById("principal").value;
+{   
+
+    var principal = document.getElementById("principal");
+
+
+    if(principal.value < 0){
+        alert("Please enter a positive amount!");
+        return;
+    }
+
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
 
-    var interest = rate*years*principal / 100;
+    var interest = rate*years*principal.value / 100;
 
     var year = new Date().getFullYear()+parseInt(years);
 
-    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>.\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>.\<br\>"
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal.value+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>.\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>.\<br\>"
+
 }
-        
+
+
 function updateRate()
 {
     var rate = document.getElementById("rate").value;
